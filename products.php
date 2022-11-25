86ad80abe34b4b556f32eee76b071f49e056486f
AlyshaBaisac <alyshajeremybaisac13@gmail.com>
2022-11-26 03:17:51 +0800

    Images done

---
+++ products.php
@@ -0,0 +1,88 @@
+<?php
+	$arrProducts = array(
+		array(
+			'name' 		  => "Suede Moto",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "1500",
+			'product1'	  => "caramel1.jpg",
+			'product2'	  => "caramel2.jpg",
+		),
+		array(
+			'name' 		  => "Chris Leather Jacket",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "2000",
+			'product1'	  => "jacket_1.jpg",
+			'product2'	  => "jacket_2.jpg",
+		), 
+		array(
+			'name' 		  => "Rose Motor Jacket ",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "1300",
+			'product1'	  => "women_jacket1.jpg",
+			'product2'	  => "women_jacket2.jpg",
+		),
+		array(
+			'name' 		  => "Vintage Hooded Leather Jacket",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "4000",
+			'product1'	  => "hoody1.jpg",
+			'product2'	  => "hoody2.jpg",
+		),
+		array(
+			'name' 		  => "Graphic Paint Skinny Jeans",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "1000",
+			'product1'	  => "jeans1.jpg",
+			'product2'	  => "jeans2.jpg",
+		),
+		array(
+			'name' 		  => "Drawstring Joggers",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "800",
+			'product1'	  => "joggers1.jpg",
+			'product2'	  => "joggers2.jpg",
+		),
+		array(
+			'name' 		  => "Wide-Leg Pleated Pants",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "950",
+			'product1'	  => "pleatedpants1.jpg",
+			'product2'	  => "pleatedpant2.jpg",
+		),
+		array(
+			'name' 		  => "Faded Skinny Jeans",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "650",
+			'product1'	  => "menjeans1.jpg",
+			'product2'	  => "menjeans2.jpg",
+		),	
+		array(
+			'name' 		  => "Print Long-Sleeve Shirt",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "750",
+			'product1'	  => "polo1.jpg",
+			'product2'	  => "polo2.jpg",
+		),		
+		array(
+			'name' 		  => "Koi Fish Graphic Baby Tee",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "850",
+			'product1'	  => "longsleeves1.jpg",
+			'product2'	  => "longsleeves2.jpg",
+		),		
+		array(
+			'name' 		  => "Graphic Tee",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "650",
+			'product1'	  => "tshirt_boy1.jpg",
+			'product2'	  => "tshirt_boy2.jpg",
+		),		
+		array(
+			'name' 		  => "Oversized Nirvana Graphic Tee",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'product_price'		  => "650",
+			'product1'	  => "tshirt_girl1.jpg",
+			'product2'	  => "tshirt_girl2.jpg",
+		),																				
+	);
+  ?>
